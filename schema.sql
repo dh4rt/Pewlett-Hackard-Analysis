b4fc44b08d7b352aa01822dfb8d1622d8bc52033
dh4rt <davidmmhart@gmail.com>
2022-12-18 19:54:56 -0500

    7.3.1 instructed push

--- schema.sql
+++ schema.sql
@@ -38,9 +38,9 @@ CREATE TABLE dept_manager (
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE dept_emp
-	dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -48,12 +48,71 @@ CREATE TABLE dept_emp
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
+DROP TABLE dept_emp
+
+COPY dept_emp FROM 'Data/dept_emp.csv' DELIMITER ',' CSV HEADER;
+
+
 SELECT * FROM departments;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
+
+
+-- 7.3.1 count the queires
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- query to determine retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--7.3.1 instructions
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+--7.3.1 skill drill
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+--7.3.1 creating a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- to pull from this new table
+SELECT * FROM retirement_info;
